f037c604217a950bfcf1d682b6aad626c033fb77
Prince Gupta <princegupta4171@gmail.com>
2026-08-11 10:34:34 +0530

    completed with error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const errorHandler = require("../middleware/errorMiddleware");
+const errorHandler = require("./middleware/errorMiddleware");
 dotenv.config();
 connectDB();
 const app = express();
